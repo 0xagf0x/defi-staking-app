2260fb68554b74c9f27ace37d982f5efa7a73b4d
Charles Buckley <charles.buckley@mobelux.com>
2021-11-01 18:01:33 -0700

    transfer func

--- src/contracts/Tether.sol
+++ src/contracts/Tether.sol
@@ -6,15 +6,42 @@ contract Tether {
     string public symbold = 'USDT';
     uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens 
     uint8 public decimals = 18;
-//     address public owner;
+    address public owner;
    
 
-//    constructor() public {
-//         owner = msg.sender;
-//    }
+   constructor() public {
+        owner = msg.sender;
+        balanceOf[msg.sender] = totalSupply;
+   }
 
-//    modifier onlyOwner() {
-//        if (msg.sender == owner) _;  // if the sender is the owner, then continue;
-//    }
+   modifier onlyOwner() {
+       if (msg.sender == owner) _;  // if the sender is the owner, then continue;
+   }
 
+   event Transfer(
+       address indexed _from,
+       address indexed _to,
+       uint _value,
+   ); 
+
+   event Approve(
+        address indexed _owner,
+        address indexed _spender,
+        uint _value,
+   );
+
+   mapping(address => uint) public balanceOf;
+
+
+   function transfer(address _to, uint _value) public  returns(bool success)  {
+       // require that the sender has enough money to send 
+       require(balanceOf[msg.sender] >= _value)
+       // remove value from sender
+       balanceOf[msg.sender] -= _value;
+       // add value to receiver 
+       balanceOf[_to] += _value;
+       // run the transfer and return it "true"
+       emit Transfer(_from, _to, _value);
+       return true;
+   }
 }
